3b97ce25e42a76f515cf17b4d605ba4039f2e98c
Nawed Khan <hp@DESKTOP-HJ5R41N>
2025-08-20 18:58:52 +0530

    Add

--- GitTestProj01/src/main/java/com/nt/sbean/Student.java
+++ GitTestProj01/src/main/java/com/nt/sbean/Student.java
@@ -4,4 +4,7 @@ public class Student {
 	public void addStudentData() {
 		System.out.println("Adding a new Student !!!");
 	}
+	public void addStudentDelete() {
+		System.out.println("Delete a Student !!!");
+	}
 }
